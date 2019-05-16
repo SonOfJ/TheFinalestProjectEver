14f4ba101eed78938f623dd4aca4f02c6f23e8ec
Jason Zheng <zzheng3@stuy.edu>
2019-05-16 14:48:58 -0400

    Unfinished.

---
+++ Maze.java
@@ -0,0 +1,12 @@
+class Maze {
+  Node upNode;
+  Node downNode;
+  Node leftNode;
+  Node rightNode;
+  Maze(Node newUp, Node newDown, Node newLeft, Node newRight) {
+  }
+  display() {
+  }
+  addNode() {
+  }
+}
