bcaaa683ac40f832cb441b756e9bf68f579833ed
Christy Guan <cguan00@stuy.edu>
2019-05-18 12:37:14 -0400

    wrote constructor and addNode method

--- Maze.java
+++ Maze.java
@@ -3,10 +3,30 @@ class Maze {
   Node downNode;
   Node leftNode;
   Node rightNode;
+
   Maze(Node newUp, Node newDown, Node newLeft, Node newRight) {
+    upNode = newUp;
+    downNode = newDown;
+    leftNode = newLeft;
+    rightNode = newRight;
   }
+
   display() {
   }
-  addNode() {
+
+  addNode(Node newNode, String direction) {
+    if(direction.equals("up")){
+      upNode = newNode;
+    }
+    if(direction.equals("down")){
+      downNode = newNode;
+    }
+    if(direction.equals("left")){
+      leftNode = newNode;
+    }
+    if(direction.equals("right")){
+      rightNode = newNode;
+    }
+    
   }
 }
