149ca6467660327bda9c015bfc163fcf5ac77c2d
Jason Zheng <zzheng3@stuy.edu>
2019-05-21 14:41:14 -0400

    Folder for testing.

---
+++ Experiments/Ball.java
@@ -0,0 +1,12 @@
+class Ball {
+  int x;
+  int y;
+  Ball(int xcor, int ycor) {
+    x = xcor;
+    y - ycor;
+  }
+  display() {
+    fill(255, 0, 0);
+    circle(x, y, 100);
+  }
+}
---
+++ Experiments/Runner/Runner.pde
@@ -0,0 +1,7 @@
+Ball b = new Ball(500, 400);
+void setup() {
+  size(1000, 800);
+}
+void draw() {
+  b.display();
+}
