5cfc7686e24294145cc16190ade44be0f60f48da
Jason Zheng <zzheng3@stuy.edu>
2019-05-21 10:25:38 -0400

    Worked on constructor.

--- Maze.java
+++ Maze.java
@@ -2,30 +2,22 @@ import java.util.*;
 import java.io.*;
 class Maze {
   Node start;
-
-  Maze(Node newUp, Node newDown, Node newLeft, Node newRight) {
-    upNode = newUp;
-    downNode = newDown;
-    leftNode = newLeft;
-    rightNode = newRight;
-  }
-
-  display() {
-  }
-
-  addNode(Node newNode, String direction) {
-    if(direction.equals("up")){
-      upNode = newNode;
+  Maze() throws FileNotFoundException {
+    File text = new File("Map.txt");
+    Scanner inf = new Scanner(text);
+    int height = 0;
+    while (inf.hasNextLine()) {
+      String line = inf.nextLine();
+      height = height + 1;
     }
-    if(direction.equals("down")){
-      downNode = newNode;
+    inf = new Scanner("Map.txt");
+    for (int i = 0; i < height; i = i + 1) {
+      String line = inf.nextLine();
+      for (int j = 0; j < line.length(); j = j + 1) {
+        if (i == 0) {
+          if (j == 0) {
+            Node n 
+          }
+        }
+      }
     }
-    if(direction.equals("left")){
-      leftNode = newNode;
-    }
-    if(direction.equals("right")){
-      rightNode = newNode;
-    }
-
-  }
-}
